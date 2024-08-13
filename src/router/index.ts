4604f7ce5030e01e87f8f04d0b13385be366c2fa
xzh <xzh.0804@qq.com>
2024-08-13 16:04:32 +0800

    登录页面准备

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   //默认参数'/' 基础路径
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { path: '/login', component: () => import('@/views/login/index.vue') }
+  ]
 })
 
 export default router
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="app">
-    <router-view></router-view>
-  </div>
+  <div class="login-page">login</div>
 </template>
 <script lang="scss"></script>
