e72023e48a82133c23949a9633eb235bd58bdc7b
xzh <xzh.0804@qq.com>
2024-08-13 19:15:25 +0800

    cp-nav-bar 功能和组件类型

--- src/components/CpNavBar.vue
+++ src/components/CpNavBar.vue
@@ -1,8 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+//  通过props  来实现标题右侧文字设置
+defineProps<{
+  title?: string
+  rightText?: string
+}>()
+
+//使用 emit函数来触发自定义事件
+const emit = defineEmits<{
+  (e: 'click-right'): void
+}>()
+const onClickRight = () => {
+  emit('click-right')
+}
+
+// 回退功能实现 history.state
+const router = useRouter()
+const onClickLeft = () => {
+  if (history.state?.back) {
+    router.back()
+  } else {
+    router.push('/')
+  }
+}
+</script>
 
 <template>
   <!-- 固定定位 左侧箭头 标题 右侧文字 -->
-  <van-nav-bar fixed left-arrow title="登录" right-text="注册"></van-nav-bar>
+  <van-nav-bar
+    fixed
+    left-arrow
+    @click-left="onClickLeft"
+    :title="title"
+    :right-text="rightText"
+    @click-right="onClickRight"
+  ></van-nav-bar>
 </template>
 
 <style lang="scss" scoped>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const fn = () => {
+  console.log('fn')
+}
+</script>
 <template>
   <div class="login-page">
-    <cp-nav-bar></cp-nav-bar>
+    <cp-nav-bar title="登录" rightText="注册" @click-right="fn"></cp-nav-bar>
     login
   </div>
 </template>
