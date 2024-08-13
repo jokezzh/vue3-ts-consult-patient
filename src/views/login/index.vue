db1372d6f3c857d0ef8031106b0512575f9f1e22
xzh <xzh.0804@qq.com>
2024-08-13 16:22:32 +0800

    组件自动注册

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="login-page">login</div>
+  <div class="login-page">
+    login
+    <van-button> an</van-button>
+  </div>
 </template>
 <script lang="scss"></script>
