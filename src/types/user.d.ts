3cc14c2df75da53bb66200f6196ad6db8f33b588
xzh <xzh.0804@qq.com>
2024-08-14 17:04:35 +0800

    个人中心用户信息渲染

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -11,6 +11,7 @@ export type User = {
   /** 头像 */
   avatar: string
 }
+
 // 短信验证码类型，登录|注册|修改手机号|忘记密码|绑定手机号
 export type CodeType =
   | 'login'
@@ -18,3 +19,26 @@ export type CodeType =
   | 'changeMobile'
   | 'forgetPassword'
   | 'bindMobile'
+
+// 个人信息
+type OmitUser = Omit<User, 'token'>
+export type UserInfo = OmitUser & {
+  /** 关注 */
+  likeNumber: number
+  /** 收藏 */
+  collectionNumber: number
+  /** 积分 */
+  score: number
+  /** 优惠券 */
+  couponNumber: number
+  orderInfo: {
+    /** 待付款 */
+    paidNumber: number
+    /** 待发货 */
+    receivedNumber: number
+    /** 待收货 */
+    shippedNumber: number
+    /** 已完成 */
+    finishedNumber: number
+  }
+}
