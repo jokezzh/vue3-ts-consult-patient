380970b334345c9504663e1b3390f39e4602e770
xzh <xzh.0804@qq.com>
2024-08-14 15:22:56 +0800

    布局容器和路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,24 @@ const router = createRouter({
   //默认参数'/' 基础路径
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/login', component: () => import('@/views/login/index.vue') }
+    { path: '/login', component: () => import('@/views/Login/index.vue') },
+    {
+      path: '/',
+      redirect: '/home',
+      component: () => import('@/views/Layout/index.vue'),
+      children: [
+        { path: '/home', component: () => import('@/views/Home/index.vue') },
+        {
+          path: '/article',
+          component: () => import('@/views/Article/index.vue')
+        },
+        {
+          path: '/notify',
+          component: () => import('@/views/Notify/index.vue')
+        },
+        { path: '/user', component: () => import('@/views/User/index.vue') }
+      ]
+    }
   ]
 })
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout-page">
+    <router-view></router-view>
+    <div>tab-bar</div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
