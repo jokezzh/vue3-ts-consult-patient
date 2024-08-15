c59e92403469762e88e83d16f40f7af35de1ed5a
xzh <xzh.0804@qq.com>
2024-08-15 19:53:34 +0800

    首页推荐关注医生的展示功能

--- src/services/consult.ts
+++ src/services/consult.ts
@@ -1,6 +1,15 @@
-import type { KnowledgePage, KnowledgeParams } from '@/types/consult'
+import type {
+  KnowledgePage,
+  KnowledgeParams,
+  PageParams,
+  DoctorPage
+} from '@/types/consult'
 import { request } from '@/utils/request'
 
 //获取列表数据
 export const getKnowledgePage = (params: KnowledgeParams) =>
   request<KnowledgePage>('/patient/home/knowledge', 'GET', params)
+
+//获取医生
+export const getDoctorPage = (params: PageParams) =>
+  request<DoctorPage>('/home/page/doc', 'GET', params)
